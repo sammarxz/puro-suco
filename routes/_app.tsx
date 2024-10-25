efb98c32b6c86280fe1b70a62514c7baf3a05177
Sam Marxz <sammarxzz@gmail.com>
2024-10-25 18:46:08 -0300

    project cleanup starter files

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,11 +1,11 @@
 import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
   return (
-    <html>
+    <html lang="pt-br">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>ui-design-na-pratica</title>
+        <title>UI design na prática</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
