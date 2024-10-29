72f050428e85c00042dcdbeba7f3dee418f2380c
Sam Marxz <sammarxzz@gmail.com>
2024-10-29 13:32:25 -0300

    add github user image on navigation bar

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,4 +1,5 @@
-import * as Icons from "./Icons.tsx";
+import { GitHubAvatarImg } from "@/components/GithubAvatarImage.tsx";
+import { LogOutIcon } from "@/components/Icons.tsx";
 
 export function NavigationBar(
   props: {
@@ -7,51 +8,24 @@ export function NavigationBar(
     user?: string;
   },
 ) {
-  // const items = [
-  // {
-  //   name: "Feedback",
-  //   href: "https://deno.com/blog?tag=fresh",
-  // },
-  // ];
-
   return (
     <nav class={"flex " + (props.class ?? "")} f-client-nav={false}>
       <ul class="flex items-center gap-x-2 sm:gap-4 mx-4 my-2 sm:my-6 flex-wrap lg:mx-8 2xl:mr-0">
-        {
-          /* {items.map((item) => (
-          <li key={item.name}>
-            <a
-              href={item.href}
-              class={`p-1 sm:p-2 ${
-                isHome ? "text-green-900" : "text-gray-600"
-              } hover:underline aria-[current]:font-bold`}
-            >
-              {item.name}
-            </a>
-          </li>
-        ))} */
-        }
-
-        <li class="flex items-center">
-          <a
-            href="https://github.com/sammarxz/puro-suco"
-            class="hover:text-amber-400 inline-block transition"
-            aria-label="GitHub"
-          >
-            <Icons.GitHub />
-          </a>
-        </li>
         {props.user
           ? (
-            <div class="flex gap-5">
-              <span>
-                Olá,{"  "}<span class="text-gray-600">{props.user}</span>
-              </span>
+            <div class="flex items-center gap-5">
+              <div class="flex items-center gap-2">
+                <GitHubAvatarImg login={props.user} size={32} />
+                <span>
+                  Olá,{"  "}<span class="text-gray-400">{props.user}</span>
+                </span>
+              </div>
               <a
                 href="/signout?success_url=/"
-                class="text-gray-600 hover:text-gray-900"
+                class="text-sm text-red-400 hover:text-red-500"
+                title="Sair"
               >
-                Sair
+                <LogOutIcon />
               </a>
             </div>
           )
@@ -60,7 +34,7 @@ export function NavigationBar(
               href="/signin"
               class="text-gray-600 hover:text-gray-900"
             >
-              Entrar
+              Fazer Login
             </a>
           )}
       </ul>
